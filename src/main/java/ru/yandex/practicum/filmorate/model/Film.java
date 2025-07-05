86d9ab7740d86db39a05fcc6369496d8cc98ebbd
Kermort <kermort@yandex.ru>
2025-07-05 15:05:23 +0300

    checkstyle fix

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -11,15 +11,11 @@ import java.time.LocalDate;
 @Data
 public class Film {
     private int id;
-
     private LocalDate releaseDate;
-    
     @Size(max = 200, message = "Описание фильма не может быть больше 200 символов")
     private String description;
-
     @Positive(message = "Продолжительность фильма должна быть положительным числом")
     private int duration;
-
     @NotNull(message = "Название фильма не может быть пустым")
     @NotBlank(message = "Название фильма не может быть пустым")
     private String name;
